0fd50df7f71fa2cca9c48e90ca65cf678eef06be
masc23 <email@ma-sc.eu>
2022-07-26 20:08:17 +0200

    Integrated update service

--- Frontend/src/app/pages/main-view/main-view.component.ts
+++ Frontend/src/app/pages/main-view/main-view.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { UpdateService } from 'src/app/services/update.service';
 
 
 @Component({
@@ -10,7 +11,7 @@ import { environment } from 'src/environments/environment';
     templateUrl: './main-view.component.html',
     styleUrls: [ './main-view.component.css' ]
 })
-export class MainViewComponent
+export class MainViewComponent implements OnInit
 {
     isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
         map(result => result.matches),
@@ -18,9 +19,13 @@ export class MainViewComponent
     );
 
     versionString : string = "";
+    loggedIn : boolean = true;
 
-    constructor(private breakpointObserver : BreakpointObserver)
+    constructor(private breakpointObserver : BreakpointObserver, private update : UpdateService)
     {
         this.versionString = environment.version;
     }
+
+    ngOnInit() : void
+    {}
 }
